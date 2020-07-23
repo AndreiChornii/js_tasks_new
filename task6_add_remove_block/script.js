fe03ac66f0af289bfdcc85034e1dbc8139ed2026
AndreiChornii <andrei.chornii@gmail.com>
2020-07-23 21:16:25 +0300

    add_ok

--- task6_add_remove_block/script.js
+++ task6_add_remove_block/script.js
@@ -3,11 +3,28 @@ var add_button = document.querySelector('#add');
 var block_name_id = document.querySelector('#block_name_id');
 
 var i = 0;
+var arr = [];
 
 add_button.onclick = function(){
     var h3 = document.createElement("h1");
-    h3.innerHTML = "№" + ++i + ' ' + block_name_id.value;
+    let val = block_name_id.value;
     h3.style.border = "1px solid black";
     h3.style.margin = "5px";
-    container.appendChild(h3);
+
+    let pr = 0;
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        // console.log(element);
+        // console.log(val);
+        if(element === val) pr = 1;
+    }
+    
+    // console.log(pr);
+    if(!pr){
+        arr.push(val);
+        let number = arr.length;
+        h3.innerHTML = "№" + number + ' ' + val;
+        container.appendChild(h3);
+    }
+    // console.dir(arr);
 }
